30e7920a7df2b381d1eddc78f9ced48051f62af8
SamuelStork <samuelstork@gmail.com>
2026-09-19 12:19:47 +0200

    Added AdjustContrast

--- project_files/MainWindow.axaml.cs
+++ project_files/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Controls;
+using System.Diagnostics;
 using Avalonia.Interactivity;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
@@ -337,7 +338,6 @@ namespace ImageApp
             byte[,] tempImage = new byte[inputImage.GetLength(0), inputImage.GetLength(1)];
             int w = tempImage.GetLength(0);
             int h = tempImage.GetLength(1);
-            // TODO: add your functionality and checks
             for (int x = 0; x < w; x++)
             for (int y = 0; y < h; y++)
             {
@@ -356,9 +356,25 @@ namespace ImageApp
             // create temporary grayscale image
             byte[,] tempImage = new byte[inputImage.GetLength(0), inputImage.GetLength(1)];
 
-            // TODO: add your functionality and checks
+            int w = tempImage.GetLength(0);
+            int h = tempImage.GetLength(1);
+            byte maxValue = 0;
+            byte minValue = byte.MaxValue;
+            Debug.WriteLine("Initialised");
+            for (int x = 0; x < w; x++)
+            for (int y = 0; y < h; y++)
+            {
+                maxValue = inputImage[x, y] > maxValue ? inputImage[x, y] : maxValue;
+                minValue = inputImage[x, y] < minValue ? inputImage[x, y] : minValue;
+            }
+            float contrastCoefficient = byte.MaxValue / (maxValue - minValue);
+            Debug.WriteLine($"Max Value: {maxValue} Min Value: {minValue} CC: {contrastCoefficient}");
 
-
+            for (int x = 0; x < w; x++)
+            for (int y = 0; y < h; y++)
+            {
+                tempImage[x, y] = (byte)Math.Floor((inputImage[x, y] - minValue) * contrastCoefficient);
+            }
             return tempImage;
         }
 
